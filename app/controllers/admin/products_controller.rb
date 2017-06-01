bed9479f33f8a9bda84a4e931bb3d77f31e7aaf7
Tranducthang28011996 <tieuhodo96@gmail.com>
2017-06-01 10:24:29 +0700

    image upload

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,8 +1,14 @@
 class Admin::ProductsController < ApplicationController
 	before_action :logged_in?, :is_admin?, :skiplayout
+	def new
+	
+		@product = Product.new
+	end
 
 	def create
-		
+		byebug
+		 @product = Product.new(params_products)
+		 @product.save
 	end
 
 	def edit
@@ -11,10 +17,17 @@ class Admin::ProductsController < ApplicationController
 
 	def destroy
 		
+		@product = Product.find(params[:id])
+  		@product.destroy
+
+	    respond_to do |format|
+	      format.html {}
+	      format.json { head :no_content }
+	      format.js   { render :layout => false }
+	    end
 	end
 
 	def update
-		byebug
 		@product = Product.find params[:id]
 		@product.update(params_products)
 	end
@@ -23,8 +36,10 @@ class Admin::ProductsController < ApplicationController
 		@skip_layout = true
 	end
 
+
 	private
 	def params_products
-		params.require(:product).permit(:name)
+		params.require(:product).permit(:name, :price, :disprice, :size, :color,
+										:image, :total, :subcategory_id, :trademark, :description)
 	end
 end
