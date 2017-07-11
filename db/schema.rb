8778b5044cc2b7cf25869077cb7c1aa53bb3025d
Tranducthang28011996 <tieuhodo96@gmail.com>
2017-07-11 14:33:45 +0700

    config layout1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623091246) do
+ActiveRecord::Schema.define(version: 20170711021105) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -79,6 +79,12 @@ ActiveRecord::Schema.define(version: 20170623091246) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["category_id"], name: "index_subcategories_on_category_id"
+  end
+
+  create_table "uploads", force: :cascade do |t|
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
